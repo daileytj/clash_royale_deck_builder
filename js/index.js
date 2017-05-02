97ac085c410461b88c50c7acd3afe0e294dc4e3f
Thomas Dailey <daileytj89@gmail.com>
2017-05-02 18:27:02 -0400

    setup store and router

--- js/index.js
+++ js/index.js
@@ -2,13 +2,29 @@ require('babel-polyfill');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import {Provider} from 'react-redux';
+import {Router, Route, Link, hashHistory} from 'react-router';
+import {Provider} from 'react-redux';
 // import * as actions from './actions/index';
-// import store from './stores/store';
+import store from '../js/stores/store';
+import {connect} from 'react-redux';
+import 'isomorphic-fetch';
+import Home from './views/home';
+import Build from './views/build';
+import Decks from './views/decks';
+
+const routes = (
+  <Route>
+    <Route path="/" component={Home}></Route>
+    <Route path="/build" component={Build}></Route>
+    <Route path="/decks" component={Decks}></Route>
+  </Route>
+)
 
 ReactDOM.render(
-        //<Provider store={store}>
-            <div> Hello World </div>,
-        //</Provider>,
+        <Provider store={store}>
+          <Router history={hashHistory}>
+            {routes}
+          </Router>
+        </Provider>,
         document.getElementById('app')
     )
--- js/stores/store.js
+++ js/stores/store.js
@@ -1,6 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 
-import * as reducers from './reducers/index';
+// import * as reducers from './reducers/index';
 
-export default createStore(reducers.reducer, applyMiddleware(thunk));
+// export default createStore(reducers.reducer, applyMiddleware(thunk));
+export default createStore(applyMiddleware(thunk));
