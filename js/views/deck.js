03e9d3f0008b43180b15365d2b01650ee26cc237
Thomas Dailey <daileytj89@gmail.com>
2017-05-16 18:58:39 -0400

    update deck and decks css and components

--- js/views/deck.js
+++ js/views/deck.js
@@ -3,11 +3,16 @@ import {connect} from 'react-redux';
 import Card from '../components/card';
 
 class Deck extends React.Component {
+
+  // const deck = this.props;
   render(props){
+    const {deck,title,strategy} = this.props;
     return(
       <div className = "deck">
-        <h2>A Deck</h2>
-        {this.props.cards.map((card,i) => <Card {...this.props} key = {i} i = {i} card = {card}/>)}
+        <h2>{title}</h2>
+        {deck.map((card,i) => <Card {...this.props} key = {i} i = {i} card = {card}/>)}
+        <h3>Strategy</h3>
+        <p className = "deck-strategy">{strategy}</p>
       </div>
     )
   }
