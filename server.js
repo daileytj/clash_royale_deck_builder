15fd8c74b15f32ffe076be7efa440a78d2b129ca
Thomas Dailey <daileytj89@gmail.com>
2017-05-22 19:29:39 -0400

    update to work on app locally

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('*'));
 
 app.listen(process.env.PORT || 8080, () => console.log(
   `Your app is listening on port ${process.env.PORT || 8080}`));
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: path.resolve(__dirname, './js/index'),
+    entry: path.resolve(__dirname, 'js/index'),
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
